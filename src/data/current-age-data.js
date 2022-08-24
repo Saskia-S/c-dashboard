dc7317de03775504ee1eabc48bc96fb46282c2d4
Saskia-S <saskia-s@github.com>
2022-08-24 09:21:07 +0200

    added charts

--- src/data/current-age-data.js
+++ src/data/current-age-data.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const getAgeGroupData = async function() {
-    return await axios.get('https://api.corona-zahlen.org/germany/age-groups');
+    const req = axios.get('https://api.corona-zahlen.org/germany/age-groups');
+    const res = await req;
+    /*let rawData = [];
+    rawData.push(res.data.data["A00-A04"]);
+    rawData.push(res.data.data["A05-A14"]);
+    rawData.push(res.data.data["A15-A34"]);
+    rawData.push(res.data.data["A35-A59"]);
+    rawData.push(res.data.data["A60-A79"]);
+    rawData.push(res.data.data["A80+"]);*/
+
+    return await (res.data.data);
 }
 
 export { getAgeGroupData };
--- src/main-page/line-chart.js
+++ src/main-page/line-chart.js
@@ -1,11 +1,12 @@
-import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import {LineChart, Line, ResponsiveContainer, Tooltip} from 'recharts';
 
 export default function TinyLineChart(props) {
-    //console.log('props', props.chartData)
+
     return(
         <ResponsiveContainer width="100%" height={300}>
             <LineChart width={300} height={100} data={props.chartData}>
                 <Line type="monotone" dataKey={props.chartDataKey} stroke="#8884d8" strokeWidth={2} />
+                <Tooltip />
             </LineChart>
         </ResponsiveContainer>
     )
